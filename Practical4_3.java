c9e7b827f16e881a1a7f1c8b01cdafeff8d4bf37
kushkansagara <110079442+kushkansagara@users.noreply.github.com>
2022-09-19 18:10:21 +0530

    Add files via upload

---
+++ Practical4_3.java
@@ -0,0 +1,23 @@
+public class Practical4_3 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 5;
+        int c[] = {4,5};
+        try
+        {
+            c[b] = c[1]/a;
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Arithmetic-Exception Reported By JAVA.");
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Array-Index-Out-Of Bounds-Exception Reported By JAVA.");
+        }
+        finally
+        {
+            System.out.println("Inside Finally");
+        }
+    }
+}
